04e00b4dff76cfc5e587bfc5c6cd44d5d00a4ee6
freethrow <aleksendric@gmail.com>
2022-07-11 21:40:33 +0200

    starlette middleware!

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from decouple import config
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+
+# from fastapi.middleware.cors import CORSMiddleware
+
+from starlette.middleware.cors import CORSMiddleware
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
