67c09e3a308e34ff836e1d0a8c49779a0dbc2ef0
freethrow <aleksendric@gmail.com>
2022-07-11 14:13:41 +0200

    add vercel origin

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ DB_NAME = config("DB_NAME", cast=str)
 
 
 # define origins
-origins = ["vercel.app"]
+origins = ["*"]
 
 # instantiate the app
 app = FastAPI()
@@ -23,7 +23,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
